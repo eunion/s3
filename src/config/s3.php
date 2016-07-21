df86a2b4e7532adbe822fb5f5eab12a9076fa4f5
Kify <hui.liu@timelink.cn>
2016-07-21 15:19:37 +0800

    Delete the default config

--- src/config/s3.php
+++ src/config/s3.php
@@ -2,8 +2,8 @@
 
 
 return [
-	'driver'	=>	's3',
-	'accessKey'	=>	'AKIAJ5RIZHO6K34F2ACA',
-	'secret'	=>	'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',
-	'bucket'	=>	'kify'
+	'driver'	=>	'',
+	'accessKey'	=>	'',
+	'secret'	=>	'',
+	'bucket'	=>	''
 ];
